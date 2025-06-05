ecb126a56f684caccee517af856f076b5b1f6a16
Undeadoman <Prudnikovp01@gmail.com>
2025-06-05 13:03:13 +0300

    Add files via upload

---
+++ 7/MainWindow.xaml.cs
@@ -0,0 +1,28 @@
+﻿using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Media.Animation;
+
+namespace AnimatedButtonApp
+{
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+            
+        }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            var fadeOut = new DoubleAnimation(1, 0, new Duration(TimeSpan.FromSeconds(0.3)));
+            fadeOut.Completed += (s, a) =>
+            {
+                ResultText.Text = "Зачем ты нажал?";
+                var fadeIn = new DoubleAnimation(0, 1, new Duration(TimeSpan.FromSeconds(0.3)));
+                ResultText.BeginAnimation(OpacityProperty, fadeIn);
+            };
+
+            ResultText.BeginAnimation(OpacityProperty, fadeOut);
+        }
+    }
+}
---
+++ 9/Class1.cs
@@ -0,0 +1,19 @@
+﻿
+namespace WpfApp9
+{
+    public class UserData
+    {
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public DateTime? BirthDate { get; set; }
+        public string Email { get; set; }
+        public string Phone { get; set; }
+        public bool IsReturn2 {  get; set; }
+        public string City { get; set; }
+        public bool IsReturn3 { get; set; }  
+        public string Street { get; set; }
+        public string HouseNumber { get; set; }
+        public string ApartmentNumber { get; set; }
+    }
+}
+
